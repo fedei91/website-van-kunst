2bd7c24928e86fd6c735bb4a6136db814cdc951f
Federico <fedej.izaguirre@gmail.com>
2022-04-04 20:34:29 +0200

    add navigation bar

--- js/index.js
+++ js/index.js
@@ -1,24 +1,13 @@
 "use strict";
 
-let list = document.querySelectorAll('.list');
-let itemBox = document.querySelectorAll('.itemBox');
+/* possible fix for erasing the labels
 
-for (let i = 0; i < list.length; i++) {
-    list[i].addEventListener('click', function() {
-        for (let j = 0; j < list.length; j++) {
-            list[j].classList.remove('active');
+ const labels = document.querySelectorAll('.lb-number');
+
+    labels.forEach(
+        label => {
+            label.remove();
         }
-        this.classList.add('active');
-        
-        let dataFilter = this.getAttribute('data-filter');
+    )
 
-        for (let k = 0; k < itemBox.length; k++) {
-            itemBox[k].classList.add('hide');
-
-            if (itemBox[k].getAttribute('data-item') == dataFilter || dataFilter == "werken") {
-                itemBox[k].classList.remove('hide');
-                itemBox[k].classList.add('active');
-            }
-        }
-    })
-}
+*/    
