c8f20549245941d14f47872e4cc025a4cba42662
Federico <fedej.izaguirre@gmail.com>
2022-05-10 17:59:55 +0200

    add actual events

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-"use strict";
-
 /* possible fix for erasing the labels
 
  const labels = document.querySelectorAll('.lb-number');
@@ -11,3 +9,27 @@
     )
 
 */    
+const navbar = document.getElementById('navbar_wrapper');
+                const popup = document.querySelector('.popup');
+                const close = document.querySelector('.close');
+                const kunst = document.querySelector('.kunst');
+                window.onload = function () {
+
+                        popup.style.display = "block";
+                        navbar.style.filter = "blur(5px)";
+                        
+                        navbar.style.pointerEvents = "none";
+                        kunst.style.pointerEvents = "none";
+                        // time delay for popup
+                }
+
+                close.addEventListener('click', function (e) {
+                        popup.style.display = "none";
+                        navbar.style.display = "block";
+                        navbar.style.filter = "blur(0)";
+                        kunst.style.filter = "blur(0)";
+                        kunst.style.visibility = "visible"
+                        navbar.style.pointerEvents = "auto";
+                        kunst.style.pointerEvents = "auto";
+                        navbar.style.opacity = "0.9";
+                })
